6834a0c1df71f672c6675cea5de1ef7a5fdb5749
Puru-G <211b232@juetguna.in>
2024-02-02 23:27:31 +0530

    firebase Setup completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -40,7 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-messaging:23.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -53,4 +54,10 @@ dependencies {
     implementation("com.makeramen:roundedimageview:2.3.0")
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    //Firebase
+    implementation("com.google.firebase:firebase-messaging:23.4.0")
+    implementation("com.google.firebase:firebase-firestore:24.10.1")
+
+    //MultiDex
+    implementation("androidx.multidex:multidex:2.0.1")
 }
--- app/src/main/java/com/example/securemessagingapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/securemessagingapp/activities/SignInActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.securemessagingapp.R;
 import com.example.securemessagingapp.databinding.ActivitySignInBinding;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding binding;
@@ -21,5 +26,21 @@ public class SignInActivity extends AppCompatActivity {
     private void setListners(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+//        binding.buttonSignIn.setOnClickListener(v-> addDataToFirebase());
     }
+
+//    private void addDataToFirebase(){
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String, Object> data = new HashMap<>();
+//        data.put("first_name","Lucifer");
+//        data.put("last_name","Mornigstar");
+//        database.collection("users")
+//                .add(data)
+//                .addOnSuccessListener(documentReference -> {
+//                    Toast.makeText(getApplicationContext(),"Data Inserted",Toast.LENGTH_SHORT).show();
+//                })
+//                .addOnFailureListener(Exception ->{
+//                    Toast.makeText(getApplicationContext(),Exception.getMessage(),Toast.LENGTH_SHORT).show();
+//                });
+//    }
 }
