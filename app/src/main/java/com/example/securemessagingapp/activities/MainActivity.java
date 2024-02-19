4a3534ece709f6ae5093cff225a86e440627d90c
liger02 <211b233@juetguna.in>
2024-02-19 15:52:10 +0530

    fvrsdfw

--- app/src/main/java/com/example/securemessagingapp/activities/MainActivity.java
+++ app/src/main/java/com/example/securemessagingapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.securemessagingapp;
+package com.example.securemessagingapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.securemessagingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/securemessagingapp/activities/SignUpActivity.java
+++ app/src/main/java/com/example/securemessagingapp/activities/SignUpActivity.java
@@ -3,6 +3,9 @@ package com.example.securemessagingapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.securemessagingapp.R;
 import com.example.securemessagingapp.databinding.ActivitySignInBinding;
@@ -11,7 +14,7 @@ import com.example.securemessagingapp.databinding.ActivitySignUpBinding;
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
-
+    private String encodedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,68 @@ public class SignUpActivity extends AppCompatActivity {
         setListners();
     }
     private void setListners(){
+
         binding.textSignIn.setOnClickListener(v -> onBackPressed());
+        binding.buttonSignUp.setOnClickListener(v->{
+            if(isValidSignUpDetails()){
+                signUp();
+            }
+        });
     }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message , Toast.LENGTH_SHORT).show();
+    }
+    private void signUp()
+    {
+
+    }
+    private Boolean isValidSignUpDetails(){
+        if(encodedImage==null)
+        {
+            showToast("Select profile image");
+            return false;
+        }
+        else if(binding.inputName.getText().toString().trim().isEmpty()){
+            showToast("Enter name");
+            return false;
+        }
+        else if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter Email");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Enter valid Email");
+            return false;
+        }
+        else if(binding.inputPassword.getText().toString().trim().isEmpty())
+        {
+            showToast("Enter password");
+            return false;
+        }
+        else if(binding.inputConfirmPassword.getText().toString().trim().isEmpty()){
+            showToast("Confirm your password");
+            return false;
+        }
+        else if(!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())){
+            showToast("Password & confirm password must be same");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private void loading(Boolean isloading)
+    {
+        if(isloading){
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }
+        else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
