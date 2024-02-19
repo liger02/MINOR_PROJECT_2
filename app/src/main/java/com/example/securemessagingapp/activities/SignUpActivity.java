19bd976680aeb1eb57fa5e8197af94d58410edd2
liger02 <211b233@juetguna.in>
2024-02-19 17:17:37 +0530

    fvrsdfwkbj

--- app/src/main/java/com/example/securemessagingapp/activities/SignUpActivity.java
+++ app/src/main/java/com/example/securemessagingapp/activities/SignUpActivity.java
@@ -1,25 +1,45 @@
 package com.example.securemessagingapp.activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.securemessagingapp.R;
+//import com.example.securemessagingapp.R;
 import com.example.securemessagingapp.databinding.ActivitySignInBinding;
 import com.example.securemessagingapp.databinding.ActivitySignUpBinding;
+import com.example.securemessagingapp.utilities.Constants;
+import com.example.securemessagingapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Console;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
+    private PreferenceManager preferenceManager;
     private String encodedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
         setListners();
     }
     private void setListners(){
@@ -30,6 +50,11 @@ public class SignUpActivity extends AppCompatActivity {
                 signUp();
             }
         });
+        binding.layoutImage.setOnClickListener(v->{
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            pickImage.launch(intent);
+        });
     }
 
     private void showToast(String message){
@@ -37,8 +62,60 @@ public class SignUpActivity extends AppCompatActivity {
     }
     private void signUp()
     {
-
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String,Object>user = new HashMap<>();
+        user.put(Constants.KEY_NAME, binding.inputName.getText().toString());
+        user.put(Constants.KEY_EMAIL,binding.inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString());
+        user.put(Constants.KEY_IMAGE,encodedImage);
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    loading(false);
+                    preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
+                    preferenceManager.putString(Constants.KEY_USER_ID,documentReference.getId());
+                    preferenceManager.putString(Constants.KEY_NAME,binding.inputName.getText().toString());
+                    preferenceManager.putString(Constants.KEY_IMAGE,encodedImage);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                })
+                .addOnFailureListener(exception ->{
+                    loading(false);
+                    showToast(exception.getMessage());
+                });
     }
+    private String encodeImage(Bitmap bitmap){
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight()*previewWidth/bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight , false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG,50,byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes,Base64.DEFAULT);
+    }
+    private final ActivityResultLauncher<Intent>pickImage=registerForActivityResult(
+      new ActivityResultContracts.StartActivityForResult(),
+      result ->{
+          if(result.getResultCode()==RESULT_OK){
+              if(result.getData()!=null)
+              {
+                  Uri imageUri = result.getData().getData();
+                  try{
+                      InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                      Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                      binding.imageProfile.setImageBitmap(bitmap);
+                      binding.textAddImage.setVisibility(View.GONE);
+                      encodedImage = encodeImage(bitmap);
+                  }
+                  catch (FileNotFoundException e){
+                      e.printStackTrace();
+                  }
+              }
+          }
+      }
+    );
     private Boolean isValidSignUpDetails(){
         if(encodedImage==null)
         {
