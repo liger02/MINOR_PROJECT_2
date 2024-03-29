9c0e016320ad9bb714f08355a53cde0347b9213b
liger02 <211b233@juetguna.in>
2024-03-29 14:36:01 +0530

    woring on message send and receive

--- app/src/main/java/com/example/securemessagingapp/adapters/ChatAdapter.java
+++ app/src/main/java/com/example/securemessagingapp/adapters/ChatAdapter.java
@@ -1,18 +1,86 @@
 package com.example.securemessagingapp.adapters;
 
 import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.securemessagingapp.databinding.ItemContainerReceivedMessageBinding;
 import com.example.securemessagingapp.databinding.ItemContainerSentMessageBinding;
 import com.example.securemessagingapp.models.ChatMessage;
+import java.util.List;
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    private final List<ChatMessage> chatMessages;
+private final Bitmap receiverProfileImage;
+private final String senderId;
+public static final int VIEW_TYPE_SENT = 1;
+public static final int VIEW_TYPE_RECEIVED = 2;
 
-public class ChatAdapter {
-    //private final List<ChatMessage> chatMessage;
-//private final Bitmap receiverProfileImage;
-//private final String senderId;
- static class SentMessageViewHolder extends RecyclerView.ViewHolder{
+
+    public ChatAdapter(List<ChatMessage> chatMessages, Bitmap receiverProfileImage, String senderId) {
+        this.chatMessages = chatMessages;
+        this.receiverProfileImage = receiverProfileImage;
+        this.senderId = senderId;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == VIEW_TYPE_SENT)
+        {
+            return new SentMessageViewHolder(
+                    ItemContainerSentMessageBinding.inflate(
+                            LayoutInflater.from(parent.getContext()),
+                            parent,
+                            false
+                    )
+            );
+        }
+        else
+        {
+            return new ReceivedMessageViewHolder(
+                    ItemContainerReceivedMessageBinding.inflate(
+                            LayoutInflater.from(parent.getContext()),
+                            parent,
+                            false
+                    )
+            );
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(getItemViewType(position)==VIEW_TYPE_SENT)
+        {
+            ((SentMessageViewHolder)holder).setData(chatMessages.get(position));
+        }
+        else
+        {
+            ((ReceivedMessageViewHolder)holder).setData(chatMessages.get(position),receiverProfileImage);
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+    public int getItemViewType(int position)
+    {
+        if(chatMessages.get(position).senderId.equals(senderId))
+        {
+            return VIEW_TYPE_SENT;
+
+        }
+        else
+        {
+           return VIEW_TYPE_RECEIVED;
+        }
+    }
+
+    static class SentMessageViewHolder extends RecyclerView.ViewHolder{
      private  final ItemContainerSentMessageBinding binding;
      SentMessageViewHolder(ItemContainerSentMessageBinding itemContainerSentMessageBinding)
      {
