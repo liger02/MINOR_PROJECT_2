925c155de0c031ea6e026855176d38be138a33fd
liger02 <211b233@juetguna.in>
2024-03-30 20:56:51 +0530

    date error is remove this is the point where the message sending process is done

--- app/src/main/java/com/example/securemessagingapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/securemessagingapp/activities/ChatActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Date;
 
 public class ChatActivity extends AppCompatActivity {
     private ActivityChatBinding binding;
