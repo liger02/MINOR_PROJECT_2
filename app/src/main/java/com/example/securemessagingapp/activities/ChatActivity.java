dd7cea901397399e81293dca9971f84254642b06
liger02 <211b233@juetguna.in>
2024-03-30 22:16:26 +0530

    Aboth parties can chat with each other but encryption is not applied, and messages are stored in Firebase.

--- app/src/main/java/com/example/securemessagingapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/securemessagingapp/activities/ChatActivity.java
@@ -4,25 +4,32 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.securemessagingapp.R;
+
 import com.example.securemessagingapp.adapters.ChatAdapter;
 import com.example.securemessagingapp.databinding.ActivityChatBinding;
 import com.example.securemessagingapp.models.ChatMessage;
 import com.example.securemessagingapp.models.User;
 import com.example.securemessagingapp.utilities.Constants;
 import com.example.securemessagingapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Date;
+import java.util.Locale;
 
 public class ChatActivity extends AppCompatActivity {
     private ActivityChatBinding binding;
@@ -46,6 +53,7 @@ public class ChatActivity extends AppCompatActivity {
         setListeners();
         loadReceiverDetails();
         init();
+        listenMessages();
     }
     private void init()
     {
@@ -72,23 +80,83 @@ public class ChatActivity extends AppCompatActivity {
         binding.inputMessage.setText(null);
 
     }
+    private void listenMessages()
+    {
+        /*HashMap<String , Object> message = new HashMap<>();
+        message.put(Constants.KEY_SENDER_ID , preferenceManager.getString(Constants.KEY_USER_ID));
+        message.put(Constants.KEY_RECEIVER_ID, receiverUser.id);
+        message.put(Constants.KEY_MESSAGE , binding.inputMessage.getText().toString());
+        message.put(Constants.KEY_TIMESTAMP, new Date());
+        database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
+        binding.inputMessage.setText(null);*/
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID , preferenceManager.getString(Constants.KEY_USER_ID))
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, receiverUser.id)
+                .addSnapshotListener(eventListener);
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID,receiverUser.id)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+    }
+    private final EventListener<QuerySnapshot> eventListener = (value, error) ->
+    {
+        if(error!=null)
+        {
+            return;
+        }
+        if(value !=null)
+        {
+            int count = chatMessages.size();
+            for(DocumentChange documentChange : value.getDocumentChanges())
+            {
+                if(documentChange.getType()==DocumentChange.Type.ADDED)
+                {
+                    ChatMessage chatMessage = new ChatMessage();
+                    chatMessage.senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                    chatMessage.receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                    chatMessage.message = documentChange.getDocument().getString(Constants.KEY_MESSAGE);
+                    chatMessage.dateTime = getReadableDateTime(documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP));
+                    chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                    chatMessages.add(chatMessage);
 
+
+                }
+            }
+            Collections.sort(chatMessages,(obj1,obj2)-> obj1.dateObject.compareTo(obj2.dateObject));
+            if(count ==0)
+            {
+                chatAdapter.notifyDataSetChanged();
+            }
+            else
+            {
+              chatAdapter.notifyItemRangeInserted(chatMessages.size(),chatMessages.size());
+              binding.chatRecyclerView.smoothScrollToPosition(chatMessages.size()-1);
+            }
+            binding.chatRecyclerView.setVisibility(View.VISIBLE);
+        }
+        binding.progressBar.setVisibility(View.GONE);
+    } ;
     private Bitmap getBitmapFromEncodedString(String encodedImage)
     {
         byte[] bytes = Base64.decode(encodedImage,Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
     }
 
-    private void loadReceiverDetails() {
+    private void loadReceiverDetails()
+    {
         receiverUser = (User)getIntent().getSerializableExtra(Constants.KEY_USER);
         binding.textName.setText(receiverUser.name);
     }
 
-    private void setListeners() {
+    private void setListeners()
+    {
         binding.imageBack.setOnClickListener(v->OnBackPressed());
         binding.layoutSend.setOnClickListener(v -> sendMessage());
     }
-
+    private String getReadableDateTime(Date date)
+    {
+        return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
+    }
     private void OnBackPressed() {
     }
 
--- app/src/main/java/com/example/securemessagingapp/models/ChatMessage.java
+++ app/src/main/java/com/example/securemessagingapp/models/ChatMessage.java
@@ -1,6 +1,7 @@
 package com.example.securemessagingapp.models;
-
+import java.util.Date;
 public class ChatMessage {
     public String senderId , receiverId , message , dateTime;
+    public Date dateObject;
 
 }
